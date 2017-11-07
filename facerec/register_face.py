94a566f7ef6c6f1f2a999a0ac6d12c2bc887b2b7
Erlend <erlend061090@gmail.com>
2017-11-07 16:05:40 +0100

    scriptet registration and made a wrapper class

---
+++ facerec/register_face.py
@@ -0,0 +1,44 @@
+import os
+import cv2
+from db import smartmirrordb
+
+camera_port = 0
+# Number of frames to throw away while the camera adjusts to light levels
+ramp_frames = 30
+# Setting camera to the given camera port
+camera = cv2.VideoCapture(camera_port)
+
+db = smartmirrordb.UserDB()
+
+
+# Function to capture image from camera
+def get_image():
+    retval, im = camera.read()
+    return im
+
+
+def add_face(id):
+    for i in range(ramp_frames):
+        temp = get_image()
+    print('Taking image..')
+    n = 1
+    return_path = None
+    if __name__ == '__main__':
+        if not os.path.isdir("training-data/s" + str(id)):
+            os.mkdir("training-data/s" + str(id))
+        return_path = "training-data/s" + str(id)
+    else:
+        if not os.path.isdir("facerec/training-data/s" + str(id)):
+            os.mkdir("facerec/training-data/s" + str(id))
+        return_path = "facerec/training-data/s" + str(id)
+    while n < 21:
+        print('Taking image')
+        camera_capture = get_image()
+        if __name__ == '__main__':
+            file = "training-data/s" + str(id) + "/" + str(n) + ".png"
+        else:
+            file = "facerec/training-data/s" + str(id) + "/" + str(n) + ".png"
+
+        cv2.imwrite(file, camera_capture)
+        n = n + 1
+    return return_path
